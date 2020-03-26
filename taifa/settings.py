66f92259a9ac446b4c698d5c774d3e2650ff2d6f
Davis <davisochieng4@gmail.com>
2020-03-25 23:48:36 -0700

    Added bootstrap support

--- taifa/settings.py
+++ taifa/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static-root' )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media-root' )
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static', 'dirs'),)
